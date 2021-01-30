99ffda8c750490369554e448a56dd05dd52dedb0
niyati salot <170120107148@git.org.in>
2021-01-30 11:29:48 +0530

    Add project files.

---
+++ User_defined_class/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace User_defined_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int rollno, semester;
+            //string name;
+            //Console.WriteLine("Enter rollno, name and semester :");
+            //rollno = Convert.ToInt32(Console.ReadLine());
+            //name = Console.ReadLine();
+            //semester = Convert.ToInt32(Console.ReadLine());
+            //Console.Write($"rollno = {rollno} \n name = {name} \n semester = {semester}");
+            Student s1 = new Student();
+            s1.rollno = 1;
+            s1.name = "Niyati";
+            s1.semester = 8;
+            s1.printData();
+        }
+    }
+}
---
+++ User_defined_class/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User_defined_class
+{
+    class Student
+    {
+        public int rollno, semester;
+        public string name;
+        public void printData()
+        {
+            Console.Write($"rollno = {rollno}  \n name = {name} \n semester = {semester}");
+        }
+    }
+}
